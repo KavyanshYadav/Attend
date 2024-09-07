dfffeeb148eeee1131801f8aa23b7aa83b0e3644
KavyanshYadav <kavyansh.yadav.dev@gmail.com>
2024-09-07 14:12:10 +0530

    ds

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  redirect("/login")
   return (
     <div>
       <h1>name</h1>
-      <Link href="/login"></Link>
+      <Link href="/dashboard">Dashboard</Link>
     </div>
   );
 }
