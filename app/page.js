b6a6df569703c3cb5e1d3bc701a7c1848e2fe3d1
KavyanshYadav <kavyansh.yadav.dev@gmail.com>
2024-09-07 14:09:15 +0530

    dsd

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h1>name</h1>
+      <Link href="/login"></Link>
     </div>
   );
 }
