b819f0b8940d43cdc83edf681e004f53840bc3c5
KavyanshYadav <kavyansh.yadav.dev@gmail.com>
2024-09-23 21:33:40 +0530

    dsds

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from 'next-auth/providers/github';
-
-
+import { use } from "react";
+import { db } from "@/lib/Firebase";
+import { collection, query, where, getDocs, addDoc } from "firebase/firestore"; // Make sure getDocs is imported
 export const authOptions = {
   providers: [
     GoogleProvider({
@@ -16,12 +17,32 @@ export const authOptions = {
     }),
 
   ],
-  // Optional: Customize your NextAuth configuration
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    async session({ session, token }) {
-      // Customize session here
-      return session;
+    async signIn(user, account, profile) {
+      try {
+        const userEmail = user.user.email;
+        const usersCollection = collection(db, "users");
+        const q = query(usersCollection,where("email","==",userEmail))
+
+        const userDocSnapshot = await getDocs(q);
+
+        if (userDocSnapshot.empty)   {
+          await addDoc(usersCollection, {
+            email: userEmail,
+            name: user.user.name,
+            image: user.user.image,
+            createdAt: new Date(),
+          });
+          console.log("created a new user",userDocSnapshot.empty)
+        } else {
+          console.log("User already exists:");
+        }
+        return true;
+      } catch (error) {
+        console.error("Error saving user to Firestore:", error);
+        return false;
+      }
     },
   },
 };
